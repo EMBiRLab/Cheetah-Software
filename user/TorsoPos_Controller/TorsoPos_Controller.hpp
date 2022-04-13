fe8a1cc96059ba3635097f3e12b68a41f75accb3
ursk <ursk@umich.edu>
2022-04-13 15:14:11 -0400

    Added quick graceful start code to torsopos

--- user/TorsoPos_Controller/TorsoPos_Controller.hpp
+++ user/TorsoPos_Controller/TorsoPos_Controller.hpp
@@ -8,7 +8,13 @@ class TorsoPos_Controller : public RobotController {
 public:
   TorsoPos_Controller():RobotController(){
     desired_q << -0.05, -0.5, 1.0, 0.05, -0.5, 1.0, -0.05, -0.5, 1.0, 0.05, -0.5, 1.0;
+    home_q = desired_q;
     // computeLegJacobianAndPosition
+
+    max_setpoint_speed_mag_rad_s = 0.05;
+    max_setpoint_delta_mag_rad
+      = max_setpoint_speed_mag_rad_s * _controlParameters->controller_dt;
+
   }
   virtual ~TorsoPos_Controller(){}
 
@@ -24,8 +30,18 @@ protected:
   Vec12<float> desired_q;
   Vec12<float> desired_joint_qd;
   Vec12<float> desired_torso_qd;
+
+  // Vec12<float> clamped_q;
+  Vec12<float> home_q;
+  bool home_pos_initialized = false;
+
+  float max_setpoint_speed_mag_rad_s = 0;
+  float max_setpoint_delta_mag_rad = 0;
+  
   Vec2<float> joystickLeft, joystickRight;
   TorsoPosUserParameters userParameters;
+
+  Vec2<float> clamp_setpoints(float nominal_qdes_rad, float nominal_qddes_rad, float cur_q);
 };
 
 #endif
